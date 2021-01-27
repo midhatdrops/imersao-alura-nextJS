6559e926494296ba524cc0997c0753665d25a3f3
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-27 13:48:37 -0300

    :recycle: Refactoring Code
    Refactored QuizContainer

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,13 @@
 import React from "react";
-import styled from "styled-components";
 import db from "../db.json";
 import Widget from "../src/Components/Widget/index";
 import Footer from "../src/Components/Footer/index";
 import GitHubCorner from "../src/Components/GitHubCorner/index";
 import QuizBackground from "../src/Components/QuizBackground/index";
 import QuizLogo from "../src/Components/QuizLogo/index";
+import QuizContainer from "../src/Components/QuizContainer";
 
 import Form from "../src/Components/Form/index";
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   return (
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,5 +1,40 @@
-import React from "react"
+import React from "react";
+
+import db from "../db.json";
+import Widget from "../src/Components/Widget/index";
+import Footer from "../src/Components/Footer/index";
+import GitHubCorner from "../src/Components/GitHubCorner/index";
+import QuizBackground from "../src/Components/QuizBackground/index";
+import QuizLogo from "../src/Components/QuizLogo/index";
+import QuizContainer from "../src/Components/QuizContainer";
+
+import Form from "../src/Components/Form/index";
 
 export default function QuizzPage() {
-  return <div>Página de Quiz</div>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <Form />
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da Galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+    </QuizBackground>
+  );
 }
