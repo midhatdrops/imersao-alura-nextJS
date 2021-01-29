d5e8b9c790a7b36c0ba8b681ad0a5fc1e70a678e
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-29 15:54:05 -0300

    :art: ID JS page
    Fixed ID JS page

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,5 +1,26 @@
+/* eslint-disable no-console */
 import React from "react";
+import QuizPage from "./index";
 
-export default function QuizDaGaleraPage() {
-  return <div>Desafio da próxima aula junto com as animações</div>;
+export default function QuizDaGaleraPage({ dbExterno }) {
+  return <QuizPage db={dbExterno} />;
+}
+
+// eslint-disable-next-line no-unused-vars
+export async function getServerSideProps(context) {
+  const dbExterno = await fetch(
+    "https://aluraquiz-base.alura-challenges.vercel.app/api/db"
+  )
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error("Falha em pegar os dados");
+    })
+    .then((res) => res)
+    .catch((err) => console.log(err));
+
+  return {
+    props: { dbExterno }, // will be passed to the page component as props
+  };
 }
